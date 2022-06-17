d7ce7c94101303df546278a04089bfc9ef592578
Edosicca <47154408+sichej@users.noreply.github.com>
2022-06-17 17:13:57 +0200

    Update config.php

--- backend/php/config.php
+++ backend/php/config.php
@@ -3,13 +3,14 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "Sbet";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, "Sbet");
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-} else
-  echo "Connected successfully";
+} 
+  #echo "Connected successfully";
 ?>
