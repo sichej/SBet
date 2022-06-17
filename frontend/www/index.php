dcee366b01b916a77475f3791839f430ac31bedb
Edosicca <47154408+sichej@users.noreply.github.com>
2022-06-17 12:25:18 +0200

    Create index.php

---
+++ frontend/www/index.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <title>Sbet</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/script.js"></script>
+        <link rel="stylesheet" href="./css/index.css">
+    </head>
+
+    <body>
+        <div class="split left">
+            <div class="centered">
+                <h2>Jane Flex</h2>
+                <p>Some text.</p>
+            </div>
+    </div>
+
+    <div class="split right">
+        <div class="bet-title">
+            <h2>Bet</h2>
+        </div>
+        <div class="centered">
+            <h2>John Doe</h2>
+            <p>Some text here too.</p>
+        </div>
+        <div class="bet-footer">
+            <a>quote: </a> 
+            <a id="total-quote">7</a><br>
+            <a>amout: </a> 
+            <a id="amount">7</a>
+        </div>
+    </div>
+        <?php 
+        session_start();
+        #echo $_SESSION['username'];
+        ?>
+
+    </body>
+</html>
