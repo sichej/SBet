7114e5f1f70d57184931df2653b8ba79f42e0cb3
Edosicca <47154408+sichej@users.noreply.github.com>
2022-06-23 17:36:02 +0200

    Update addBet.php

--- backend/php/addBet.php
+++ backend/php/addBet.php
@@ -6,6 +6,10 @@ if(!isset($_SESSION['betted_games'])){
     $_SESSION['betted_games'] = array();
 }
 
+if(!isset($_SESSION['total_quote'])){
+    $_SESSION['total_quote'] = 1;
+}
+
 if (isset($_POST['id_game']) && isset($_POST['result'])) {
     $id_game = $_POST['id_game'];
     $m_result = $_POST['result'];
@@ -14,10 +18,18 @@ if (isset($_POST['id_game']) && isset($_POST['result'])) {
     $sql = "SELECT * FROM game WHERE id_game = '$id_game'";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
-        # if match exists
-        #array_push($_SESSION['betted_games'], $id_game);
-        array_push($_SESSION['betted_games'], $id_game);
-        array_push($_SESSION['betted_games'], $m_result);
+        # if match exists and it's not already betted
+        if(!in_array($id_game, $_SESSION['betted_games'])){
+            array_push($_SESSION['betted_games'], $id_game);
+            array_push($_SESSION['betted_games'], $m_result);
+
+            $sql = "SELECT * FROM quote WHERE id_game = '$id_game'";
+            $result = $conn->query($sql);
+            $row = $result->fetch_assoc();
+            $_SESSION['total_quote'] *= $row[$m_result];
+     
+            header('Location: ../../frontend/www/calcio.php');
+        }
         header('Location: ../../frontend/www/calcio.php');
     }else{
         # if doesn't exist
