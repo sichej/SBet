233fee84ba0228fffef67ba677f6e23aa6a343a4
Edosicca <47154408+sichej@users.noreply.github.com>
2022-06-27 09:56:09 +0200

    Update bet.php

--- backend/php/bet.php
+++ backend/php/bet.php
@@ -2,7 +2,62 @@
 include('../../backend/php/config.php');
 session_start();
 
-if (isset($_POST['total_amount']) && isset($_POST['result'])) {
+# generate random string for id_bet
+function generateRandomString($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+if (isset($_POST['total_amount'])) {
+    $total_amount = $_POST['total_amount'];
+    $username = $_SESSION['username'];
+    $money = 0;
+    echo $total_amount;
+    
+    #check amount betted is regular
+    if ($total_amount < 2) exit;
+    if ($total_amount > 9999) exit;
+
+    $id_bet = generateRandomString(16);
+    $total_quote = round($_SESSION['total_quote'],2);
+
+    # check if user's money are enought for the bet
+    $sql = "SELECT money FROM user WHERE username = '$username'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $money = $row['money'];
+            if ($money < $total_amount){
+                # not enough money
+                header('Location: ../../frontend/www/index.php');
+            }
+        }
+    }
+
+    # insert bet into db
+    $sql = "INSERT INTO bet (id_bet, total_quote, amount_betted) VALUES ('$id_bet', '$total_quote', '$total_amount')";
+    if ($conn->query($sql) === TRUE) {
+        echo "Betted";
+        # insert link bet to user
+        $sql = "INSERT INTO bet_user (id_bet, username) VALUES ('$id_bet', '$username')";
+        if ($conn->query($sql) === TRUE){
+            # update money on user profile
+            $updated_money = $money - $total_amount;
+            $sql = "UPDATE user SET money = ' $updated_money' WHERE username = '$username'";
+            if ($conn->query($sql) === TRUE){
+                $_SESSION['total_quote'] = 0;
+                $_SESSION['betted_games'] = array();
+                header('Location: ../../frontend/www/index.php');
+            }
+        }
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
     
 }
 ?>
